a73ac4932ddfed44654895e630dd18730caffcba
jonberenguer <berenguer.jon@gmail.com>
2020-11-02 01:07:22 +0800

    update ytdl scripts

--- youtube-dl/get-video.sh
+++ youtube-dl/get-video.sh
@@ -4,12 +4,14 @@ VID=$1
 
 OUTPUT=$(youtube-dl --get-filename \
  --no-call-home \
+ -f 'bestvideo[height<=720]+bestaudio/best[height<=720]' \
  -o '/home/youtube-dl/Downloads/%(title)s.mp4' \
  $VID)
 
 
 [ -f "$OUTPUT" ] && echo "File exists" || youtube-dl \
  --no-call-home \
+ -f 'bestvideo[height<=720]+bestaudio/best[height<=720]' \
  -o '/home/youtube-dl/Downloads/%(title)s.mp4' \
  $VID
 
